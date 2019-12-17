82a2812de6e93deb0e3894e92f6e7a51335d2a88
KurryLuo <kurryluo@yahoo.com>
2019-12-17 22:21:18 +0800

    add some examples

--- src/AlignCenter/index.js
+++ src/AlignCenter/index.js
@@ -4,8 +4,8 @@ import "./index.less"
 export default class AlignCenter extends React.Component{
     render(){
         return (
-            <div>
-                <div className="first">第一栏</div>
+            <div className="parent">
+                <div className="child">第一栏</div>
             </div>
         )
     }
